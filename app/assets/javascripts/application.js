41b2dc6aa951b9063eefecee2757d2970c81599b
Matt King <mattdanking@gmail.com>
2016-03-30 01:45:31 -0400

    table search

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,6 +15,9 @@
 //= require turbolinks
 //= require_tree .
 
+
+// Navigation Bar //
+
 $(window).resize(function() {
   var path = $(this);
   var contW = path.width();
@@ -42,4 +45,33 @@ $(document).ready(function() {
       document.getElementsByClassName("sidebar-toggle")[0].style.left="200px";
     }
   });
+});
+
+// Patient History Table //
+
+$(document).ready(function() {
+  $(".search").keyup(function () {
+    var searchTerm = $(".search").val();
+    var listItem = $('.results tbody').children('tr');
+    var searchSplit = searchTerm.replace(/ /g, "'):containsi('")
+    
+  $.extend($.expr[':'], {'containsi': function(elem, i, match, array){
+        return (elem.textContent || elem.innerText || '').toLowerCase().indexOf((match[3] || "").toLowerCase()) >= 0;
+    }
+  });
+    
+  $(".results tbody tr").not(":containsi('" + searchSplit + "')").each(function(e){
+    $(this).attr('visible','false');
+  });
+
+  $(".results tbody tr:containsi('" + searchSplit + "')").each(function(e){
+    $(this).attr('visible','true');
+  });
+
+  var jobCount = $('.results tbody tr[visible="true"]').length;
+    $('.counter').text(jobCount + ' item');
+
+  if(jobCount == '0') {$('.no-result').show();}
+    else {$('.no-result').hide();}
+      });
 });

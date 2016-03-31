3ba1695ed3fb6c2ea6c225e7ebdddc87470b3c9e
Matt King <mattdanking@gmail.com>
2016-03-31 02:44:43 -0400

    nested history in patient

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
 
   root "patients#index"
-  resources :histories
-  resources :patients
+  resources :patients do
+    resources :histories
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
